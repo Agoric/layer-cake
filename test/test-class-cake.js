777313de2ca449b03ea9221176dfbe520b8a0817
David Bruant <bruant.d@gmail.com>
2020-01-09 17:27:47 +0100

    Solution without generators (#4)
    * Solution without generators
    
    * Adjust readme to describe the new generator-less pattern
    
    * Address @erights comments
    
    * Fix main/module/browser in package.json

--- test/test-class-cake.js
+++ test/test-class-cake.js
@@ -2,10 +2,10 @@ import test from 'tape';
 import harden from '@agoric/harden';
 import { makeClassCake } from '../lib/layer-cake';
 
-test('cajita-wobbly-point-class test', t => {
+test('hardened-wobbly-point-class test', t => {
   try {
-    function* BasePointLayer(x, y) {
-      const [self] = yield {
+    function BasePointLayer(x, y) {
+      return self => harden({
         getX() {
           return x;
         },
@@ -15,63 +15,17 @@ test('cajita-wobbly-point-class test', t => {
         toString() {
           return `<${self.getX()},${self.getY()}>`;
         },
-      };
+      });
     }
+    harden(BasePointLayer);
 
-    function* WobblyPointLayer(wobble) {
-      const [_self, supr] = yield {
+    function WobblyPointLayer(wobble) {
+      return (_self, supr) => harden({
         getX() {
           // eslint-disable-next-line no-plusplus
           return supr.getX() + wobble++;
         },
-      };
-    }
-
-    function makeWobblyPoint(x, y, wobble) {
-      return makeClassCake([BasePointLayer(x, y), WobblyPointLayer(wobble)]);
-    }
-
-    const wp1 = makeWobblyPoint(3, 5, 0.1);
-    const wp2 = makeWobblyPoint(3, 5, 0.1);
-
-    t.equal(`${wp1}`, '<3.1,5>');
-    t.equal(`${wp1}`, '<4.1,5>');
-    t.equal(`${wp2}`, '<3.1,5>');
-  } catch (e) {
-    t.isNot(e, e, 'unexpected exception');
-  } finally {
-    t.end();
-  }
-});
-
-test('hardened-wobbly-point-class test', t => {
-  try {
-    function* BasePointLayer(x, y) {
-      const [self] = harden(
-        yield harden({
-          getX() {
-            return x;
-          },
-          getY() {
-            return y;
-          },
-          toString() {
-            return `<${self.getX()},${self.getY()}>`;
-          },
-        }),
-      );
-    }
-    harden(BasePointLayer);
-
-    function* WobblyPointLayer(wobble) {
-      const [_self, supr] = harden(
-        yield harden({
-          getX() {
-            // eslint-disable-next-line no-plusplus
-            return supr.getX() + wobble++;
-          },
-        }),
-      );
+      })
     }
     harden(WobblyPointLayer);
 
--- test/test-trait-cake.js
+++ test/test-trait-cake.js
@@ -2,10 +2,11 @@ import test from 'tape';
 import harden from '@agoric/harden';
 import { makeTraitCake } from '../lib/layer-cake';
 
-test('cajita-wobbly-point-trait test', t => {
+
+test('hardened-wobbly-point-trait test', t => {
   try {
-    function* AbstractPointLayer(x, y) {
-      const [self] = yield {
+    function AbstractPointLayer(x, y) {
+      return self => harden({
         baseGetX() {
           return x;
         },
@@ -15,66 +16,17 @@ test('cajita-wobbly-point-trait test', t => {
         toString() {
           return `<${self.getX()},${self.getY()}>`;
         },
-      };
+      });
     }
+    harden(AbstractPointLayer);
 
-    function* WobblyPointLayer(wobble) {
-      const [self] = yield {
+    function WobblyPointLayer(wobble) {
+      return self => harden({
         getX() {
           // eslint-disable-next-line no-plusplus
           return self.baseGetX() + wobble++;
         },
-      };
-    }
-
-    function makeWobblyPoint(x, y, wobble) {
-      return makeTraitCake([
-        AbstractPointLayer(x, y),
-        WobblyPointLayer(wobble),
-      ]);
-    }
-
-    const wp1 = makeWobblyPoint(3, 5, 0.1);
-    const wp2 = makeWobblyPoint(3, 5, 0.1);
-
-    t.equal(`${wp1}`, '<3.1,5>');
-    t.equal(`${wp1}`, '<4.1,5>');
-    t.equal(`${wp2}`, '<3.1,5>');
-  } catch (e) {
-    t.isNot(e, e, 'unexpected exception');
-  } finally {
-    t.end();
-  }
-});
-
-test('hardened-wobbly-point-trait test', t => {
-  try {
-    function* AbstractPointLayer(x, y) {
-      const [self] = harden(
-        yield harden({
-          baseGetX() {
-            return x;
-          },
-          getY() {
-            return y;
-          },
-          toString() {
-            return `<${self.getX()},${self.getY()}>`;
-          },
-        }),
-      );
-    }
-    harden(AbstractPointLayer);
-
-    function* WobblyPointLayer(wobble) {
-      const [self] = harden(
-        yield harden({
-          getX() {
-            // eslint-disable-next-line no-plusplus
-            return self.baseGetX() + wobble++;
-          },
-        }),
-      );
+      });
     }
     harden(WobblyPointLayer);
 
