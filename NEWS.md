9f836b0c569354862f1dadf5b7033a5ebdbc97e6
Kate Sills <kate@agoric.com>
2020-01-14 11:57:28 -0800

    Prepare for Release v0.0.1 (#6)
    * prepare for release v0.0.1

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# User-visible Changes
+
+## Release v0.0.1 (1/13/2020)
+
+* Initial release
--- src/layer-cake.js
+++ src/layer-cake.js
@@ -11,27 +11,33 @@ const {
 function makeCakeMaker(combineLayers) {
   return function makeCake(layerFuns) {
     const finalObject = {};
-    const suprs = []
-    
-    for(const layerFun of layerFuns){
+    const suprs = [];
+
+    for (const layerFun of layerFuns) {
       const supr = suprs[suprs.length - 1];
       combineLayers(finalObject, layerFun(finalObject, supr));
-      const nextSupr = objCreate(objPrototype, gopds(finalObject))
-      suprs.push(nextSupr)
+      const nextSupr = objCreate(objPrototype, gopds(finalObject));
+      suprs.push(nextSupr);
     }
-    
+
     return harden(finalObject);
   };
 }
 
 // The additionalLayer overrides existing layers
-const makeClassCake = makeCakeMaker((objectInConstruction, additionalLayer = {}) =>
-  defineProperties(objectInConstruction, gopds({...additionalLayer}))
+const makeClassCake = makeCakeMaker(
+  (objectInConstruction, additionalLayer = {}) =>
+    defineProperties(objectInConstruction, gopds({ ...additionalLayer })),
 );
 
 // The layers must be disjoint
-const makeTraitCake = makeCakeMaker((objectInConstruction, additionalLayer = {}) => {
-  defineProperties(objectInConstruction, gopds(freeze({ ...additionalLayer })));
-});
+const makeTraitCake = makeCakeMaker(
+  (objectInConstruction, additionalLayer = {}) => {
+    defineProperties(
+      objectInConstruction,
+      gopds(freeze({ ...additionalLayer })),
+    );
+  },
+);
 
 export { makeCakeMaker, makeClassCake, makeTraitCake };
